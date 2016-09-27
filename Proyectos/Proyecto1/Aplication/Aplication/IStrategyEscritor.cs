32e953cc3063be54905c42f2b401099f8d6ece65
Saul Zamora <szamora@pernix-solutions.com>
2016-09-26 19:34:18 -0600

    Create proyect. Add EscribirContext. Create EsscribirTXT.

---
+++ Proyectos/Proyecto1/Aplication/Aplication/IStrategyEscritor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication
+{
+    interface IStrategyEscritor<T>
+    {
+        void Escribir(String pTexto, String pAlgoritmo, String pModo, String pResultado);
+    }
+}
