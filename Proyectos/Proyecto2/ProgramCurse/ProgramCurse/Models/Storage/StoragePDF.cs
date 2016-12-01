47e2e6cd415418a11879908c67c4547f862bb5bc
joseenriquealvaradochaves <joseenriquealvarado93@hotmail.com>
2016-12-01 10:43:32 -0600

    Guardar en PDF
    Nugget para cargar PFD
    Clase para cargar pdf

---
+++ Proyectos/Proyecto2/ProgramCurse/ProgramCurse/Models/Storage/StoragePDF.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+
+namespace ProgramCurse.Models.Storage
+{
+    public class StoragePDF
+    {
+        private String filePath = @"salida.pdf";
+
+        public void setFilePath(String pFilePath)
+        {
+            this.filePath = pFilePath;
+        }
+
+        public void Escribir(String pInfo)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    var doc = new iTextSharp.text.Document();
+                    PdfWriter.GetInstance(doc, new FileStream(filePath, FileMode.Create));
+
+                    doc.Open();
+
+                    doc.Add(new Paragraph(pInfo));
+
+                    doc.Close();
+                }
+                else
+                {
+                    Document newDoc = new Document();
+                    PdfCopy writer = new PdfCopy(newDoc, new FileStream(filePath, FileMode.Create));
+
+                    newDoc.Open();
+
+                    PdfReader reader = new PdfReader(filePath);
+                    reader.ConsolidateNamedDestinations();
+
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        PdfImportedPage page = writer.GetImportedPage(reader, i);
+                        writer.AddPage(page);
+                    }
+                    newDoc.Add(new Paragraph(pInfo));
+                   
+
+                    reader.Close();
+                    writer.Close();
+                    newDoc.Close();
+                }
+            }
+            catch (Exception error)
+            {
+                Console.Write(error.Message);
+            }
+        }
+    }
+}
