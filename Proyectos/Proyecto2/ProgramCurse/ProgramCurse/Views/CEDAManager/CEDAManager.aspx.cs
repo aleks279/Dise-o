a5f3a89fe1b3ab6a820db07bffd35bec256d197a
Ariel Herrera Fernández <carherrera@outlook.es>
2016-12-01 11:50:56 -0600

    Update 01/12/16
    Valimos...
    YOLO...

---
+++ Proyectos/Proyecto2/ProgramCurse/ProgramCurse/Views/CEDAManager/CEDAManager.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProgramCurse.Views.CEDAManager
+{
+    public partial class CEDAManager : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
